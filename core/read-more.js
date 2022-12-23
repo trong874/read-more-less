1b186d6227b2edfe2fc7fbd9cb79d8aeb22c9117
trong44 <trongpv@hqplay.vn>
2022-12-23 13:47:44 +0700

    remove jquery

--- core/read-more.js
+++ core/read-more.js
@@ -4,30 +4,36 @@ function ReadMore(selector,options = {}, lineNum = 5) {
     let class_button = !('class_button' in options) ? 'read-more' : options.class_button;
     let selector_wrap = !('selector_wrap' in options) ? '.hidden-text' : options.selector_wrap;
     let speed = !('speed' in options) ? '.3s' : options.speed;
-    let jObj = $(selector);
+    let jObj = document.querySelectorAll(selector);
 
-    Array.from(jObj).forEach( elm => {
-        let minHeight = parseInt($(elm).children(selector_wrap).css('line-height'),10)*lineNum;
+    jObj.forEach( elm => {
+        let minHeight = parseInt(getComputedStyle(elm.querySelector(selector_wrap)).lineHeight,10)*lineNum;
         let textMinHeight = ''+ minHeight +'px';
         textMinHeight = isNaN(minHeight) ? '100px' : textMinHeight;
-        let textMaxHeight = ''+$(elm).children(selector_wrap).css('height');
-        $(elm).attr('data-min_height',textMinHeight);
-        $(elm).attr('data-max_height',textMaxHeight);
-        $(elm).children(selector_wrap).css('height', ''+ textMaxHeight);
-        $(elm).children(selector_wrap).css( 'transition', 'height ' + speed);
-        $(elm).children(selector_wrap).css('height', ''+ textMinHeight);
-        $(elm).append(`<a href="javascript:;" class="toggle-content ${class_button}">${READ_MORE_LABEL}</a>`);
-    });
-    jObj.children('.toggle-content').on('click',function() {
-        let elm = $(this).closest(selector)
-        let textMinHeight = $(elm).data('min_height');
-        let textMaxHeight = $(elm).data('max_height');
-        if (elm.children(selector_wrap).css('height') === textMinHeight) {
-            elm.children(selector_wrap).css('height', ''+textMaxHeight);
-            elm.children('.toggle-content').html(HIDE_LABEL).addClass('active');
-        } else {
-            elm.children(selector_wrap).css('height', ''+textMinHeight);
-            elm.children('.toggle-content').html(READ_MORE_LABEL).removeClass('active');
+        let textMaxHeight = ''+getComputedStyle(elm.querySelector(selector_wrap)).height;
+        elm.dataset.min_height = textMinHeight;
+        elm.dataset.max_height = textMaxHeight;
+        elm.querySelector(selector_wrap).style.height = ''+ textMaxHeight;
+        elm.querySelector(selector_wrap).style.transition = 'height ' + speed;
+        elm.querySelector(selector_wrap).style.height = ''+ textMinHeight
+        if (textMaxHeight > textMinHeight) {
+            elm.innerHTML +=`<a href="javascript:;" class="toggle-content ${class_button}">${READ_MORE_LABEL}</a>`;
         }
+
+        elm.querySelector('.toggle-content').addEventListener('click',function(e) {
+            let elm = e.target.closest(selector);
+            let textMinHeight = elm.dataset.min_height;
+            let textMaxHeight = elm.dataset.max_height;
+            let button = elm.querySelector('.toggle-content');
+            if (getComputedStyle(elm.querySelector(selector_wrap)).height === textMinHeight) {
+                elm.querySelector(selector_wrap).style.height = ''+textMaxHeight;
+                button.classList.add('active');
+                button.innerHTML = HIDE_LABEL;
+            } else {
+                elm.querySelector(selector_wrap).style.height = ''+textMinHeight;
+                button.classList.remove('active');
+                button.innerHTML = READ_MORE_LABEL;
+            }
+        });
     });
 }
